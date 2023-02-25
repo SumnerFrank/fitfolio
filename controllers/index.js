2b8ed501072127f6a8cbcc8f73e95dc937d55c6e
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-02-25 09:25:55 -0500

    added error message for 404

--- controllers/index.js
+++ controllers/index.js
@@ -5,4 +5,8 @@ const homeRoutes = require('./homeRoutes')
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
+router.use((req, res) => {
+    res.status(404).end();
+});
+
 module.exports = router;
