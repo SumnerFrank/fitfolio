5ab3356d612cbaf675b52707a74c970bf57bc4b7
Your Name <JulienneJulien15@gmail.com>
2023-03-02 18:39:41 -0500

    updated main handlebars so logout button can work

---
+++ seeds/index.js
@@ -0,0 +1,22 @@
+// const seedWorkouts = require('./workout-seeds');
+// const seedPosts = require('./post-seeds');
+// const seedComments = require('./comment-seeds');
+
+// const sequelize = require('../config/connection');
+
+// const seedAll = async () => {
+//   await sequelize.sync({ force: true });
+//   console.log('--------------');
+//   await seedUsers();
+//   console.log('--------------');
+
+//   await seedWorkouts();
+//   console.log('--------------');
+
+//   await seedComments();
+//   console.log('--------------');
+
+//   process.exit(0);
+// };
+
+// seedAll();
